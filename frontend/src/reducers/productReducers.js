f2dd88f3403e73ab6aa11d1241d49b804b72226b
Kato <kubkate@gmail.com>
2021-04-20 20:31:03 -0700

    Refactor and implemment get a single product details from redux store

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -1,14 +1,18 @@
 import {
   PRODUCT_LIST_REQUEST,
   PRODUCT_LIST_SUCCESS,
-  PRODUCT_LIST_FAIL
+  PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST,
+  PRODUCT_DETAILS_SUCCESS,
+  PRODUCT_DETAILS_FAIL,
 } from '../constants/productConstants';
 
 /**
+ * @desc Get all products
  * @param {*} state - initial state
  * @param {*} action 
  */
-export const productListReducer = (state={ products: []}, action) => {
+export const productListReducer = (state = { products: []}, action) => {
   switch (action.type) {
     case   PRODUCT_LIST_REQUEST:
       return { loading: true, products: [] }
@@ -20,4 +24,23 @@ export const productListReducer = (state={ products: []}, action) => {
       return state
   }
 }
+
+/**
+ * @desc Get a sigle product
+ * @param {*} state - initial state
+ * @param {*} action 
+ */
+ export const producDetailsReducer = (state = { product: { reviews: []}}, action) => {
+  switch (action.type) {
+    case   PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state }
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload }
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload }
+    default:
+      return state
+  }
+}
+
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,11 +2,16 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-import { productListReducer } from './reducers/productReducers';
+import { 
+  productListReducer, 
+  producDetailsReducer 
+} from './reducers/productReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: producDetailsReducer,
 });
+
 const intitialState = {};
 const middleWare = [thunk];
 
